6eb13ae2623a9a32bc454edf27cd652472ee10bd
July E. Santos <july081503@gmail.com>
2025-08-30 18:03:56 -0400

    Primeiro Commit - backend agridoce cardápio e delivery app

---
+++ src/test/java/com/webapp/agridoce/AgridoceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.agridoce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgridoceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
